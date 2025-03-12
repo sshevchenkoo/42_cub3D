6be68e67e1303d2153c5b738e0f210be3e4cca54
Kvadrot <send.me.your.maild@gmail.com>
2025-03-12 12:40:32 +0100

    parse_file is refactored inside parse.c

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:04:47 by itykhono          #+#    #+#             */
-/*   Updated: 2025/03/12 12:18:14 by itykhono         ###   ########.fr       */
+/*   Updated: 2025/03/12 12:37:51 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -238,7 +238,6 @@ int	count_file_lines(const char *file_path)
 	return (lines);
 }
 
-
 int	color_floor(unsigned long floor_color)
 {
 	return ((int)floor_color);
@@ -274,46 +273,62 @@ void	set_player_position(t_data *data)
 	}
 }
 
-int	parse_file(const char *file_path, t_data *data)
+int	open_and_validate_file(const char *file_path, t_data *data)
 {
-	int	fd_map;
-
 	data->map_det.lines_file = count_file_lines(file_path);
 	if (data->map_det.lines_file < 0)
 	{
 		error_msg(ERR_MAP, 1);
 		return (0);
 	}
-	fd_map = open(file_path, O_RDONLY);
-	if (fd_map < 0)
+	data->map_det.fd = open(file_path, O_RDONLY);
+	if (data->map_det.fd < 0)
 	{
 		error_msg(ERR_FL, 2);
 		return (0);
 	}
-	data->map_det.fd = fd_map;
 	data->map_det.path = ft_strdup(file_path);
-	if (!parse_textures_and_colors(fd_map, &data->texture_det))
-	{
-		error_msg(ERR_MAP, 1);
-		close(fd_map);
-		return (0);
-	}
-	data->map = init_map(fd_map);
+	return (1);
+}
+
+int	initialize_map(t_data *data)
+{
+	data->map = init_map(data->map_det.fd);
 	if (!data->map)
 	{
 		error_msg(ERR_MAP, 1);
-		close(fd_map);
+		close(data->map_det.fd);
 		return (0);
 	}
-	close(fd_map);
+	close(data->map_det.fd);
 	data->map_det.height = calculate_map_height(data->map);
 	data->map_det.width = calculate_map_width(data->map);
 	data->map_det.start_i_map = data->map_det.lines_file - data->map_det.height;
 	data->map_det.end_i_map = data->map_det.lines_file;
+	return (1);
+}
+
+void	allocate_colors(t_data *data)
+{
 	data->texture_det.floor = malloc(sizeof(int));
 	data->texture_det.ceiling = malloc(sizeof(int));
 	*(data->texture_det.floor) = color_floor(data->texture_det.hex_floor);
 	*(data->texture_det.ceiling) = color_ceiling(data->texture_det.hex_ceiling);
+}
+
+int	parse_file(const char *file_path, t_data *data)
+{
+	if (!open_and_validate_file(file_path, data))
+		return (0);
+	if (!parse_textures_and_colors(data->map_det.fd, &data->texture_det))
+	{
+		error_msg(ERR_MAP, 1);
+		close(data->map_det.fd);
+		return (0);
+	}
+	if (!initialize_map(data))
+		return (0);
+	allocate_colors(data);
 	set_player_position(data);
 	return (1);
 }
