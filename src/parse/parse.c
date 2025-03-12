7219239302e640013e17c4df575593cfcb939f28
Kvadrot <send.me.your.maild@gmail.com>
2025-03-12 14:25:15 +0100

    parse.c parse_textures_and_colors is refactored

--- src/parse/parse.c
+++ src/parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 12:04:47 by itykhono          #+#    #+#             */
-/*   Updated: 2025/03/12 12:37:51 by itykhono         ###   ########.fr       */
+/*   Updated: 2025/03/12 14:24:09 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,54 @@ char	*trim_newline(char *str)
 	return (str);
 }
 
+int	is_texture(char *line)
+{
+	return (ft_strncmp(line, "NO ", 3) == 0 || ft_strncmp(line, "SO ", 3) == 0
+		|| ft_strncmp(line, "WE ", 3) == 0 || ft_strncmp(line, "EA ", 3) == 0);
+}
+
+int	is_color(char *line)
+{
+	return (ft_strncmp(line, "F ", 2) == 0 || ft_strncmp(line, "C ", 2) == 0);
+}
+
+int	parse_texture(char *line, t_texture_det *texture_det)
+{
+	if (ft_strncmp(line, "NO ", 3) == 0)
+		texture_det->north = ft_strdup(line + 3);
+	else if (ft_strncmp(line, "SO ", 3) == 0)
+		texture_det->south = ft_strdup(line + 3);
+	else if (ft_strncmp(line, "WE ", 3) == 0)
+		texture_det->west = ft_strdup(line + 3);
+	else if (ft_strncmp(line, "EA ", 3) == 0)
+		texture_det->east = ft_strdup(line + 3);
+	else
+		return (0);
+	return (1);
+}
+
+int	handle_color_assignment(char *line, t_texture_det *texture_det, int index)
+{
+	if (index == 4 && ft_strncmp(line, "F ", 2) == 0)
+		texture_det->hex_floor = parse_color(line + 2);
+	else if (index == 5 && ft_strncmp(line, "C ", 2) == 0)
+	{
+		texture_det->hex_ceiling = parse_color(line + 2);
+		return (1);
+	}
+	else
+		return (0);
+	return (1);
+}
+
+int	process_texture_or_color(char *line, t_texture_det *texture_det, int i)
+{
+	trim_newline(line);
+	if (i < 4)
+		return (parse_texture(line, texture_det));
+	return (handle_color_assignment(line, texture_det, i));
+}
+
 int	parse_textures_and_colors(int fd_map, t_texture_det *texture_det)
 {
 	char	*line;
@@ -60,7 +108,7 @@ int	parse_textures_and_colors(int fd_map, t_texture_det *texture_det)
 
 	i = 0;
 	line = get_next_line(fd_map);
-	while (line)
+	while (line && i < 6)
 	{
 		if (ft_strcmp(line, "\n") == 0)
 		{
@@ -68,52 +116,16 @@ int	parse_textures_and_colors(int fd_map, t_texture_det *texture_det)
 			line = get_next_line(fd_map);
 			continue ;
 		}
-		trim_newline(line);
-		if (i < 4)
+		if (!process_texture_or_color(line, texture_det, i))
 		{
-			if (ft_strncmp(line, "NO ", 3) == 0)
-				texture_det->north = ft_strdup(line + 3);
-			else if (ft_strncmp(line, "SO ", 3) == 0)
-				texture_det->south = ft_strdup(line + 3);
-			else if (ft_strncmp(line, "WE ", 3) == 0)
-				texture_det->west = ft_strdup(line + 3);
-			else if (ft_strncmp(line, "EA ", 3) == 0)
-				texture_det->east = ft_strdup(line + 3);
-			else
-			{
-				free(line);
-				return (0);
-			}
-		}
-		else if (i == 4)
-		{
-			if (ft_strncmp(line, "F ", 2) == 0)
-				texture_det->hex_floor = parse_color(line + 2);
-			else
-			{
-				free(line);
-				return (0);
-			}
-		}
-		else if (i == 5)
-		{
-			if (ft_strncmp(line, "C ", 2) == 0)
-			{
-				texture_det->hex_ceiling = parse_color(line + 2);
-				free(line);
-				return (1);
-			}
-			else
-			{
-				free(line);
-				return (0);
-			}
+			free(line);
+			return (0);
 		}
 		free(line);
 		i++;
 		line = get_next_line(fd_map);
 	}
-	return (0);
+	return (i == 6);
 }
 
 char	**init_map(int fd_map)
